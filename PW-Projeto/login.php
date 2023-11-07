16029fbd24f54455f4bc9c76cfc6ed7ee5c80ae7
Juan Pablo <75508836+pablobdss@users.noreply.github.com>
2023-11-07 19:36:27 -0300

    Add files via upload

---
+++ PW-Projeto/login.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+if (isset($_SESSION['logado'])) {
+    header('Location: menu.php');
+    exit;
+}
+
+if (isset($_POST['submit_login'])) {
+    $usuario = $_POST['usuario'];
+    $senha = $_POST['senha'];
+
+    //credenciais corretas
+    if ($usuario === 'seu_usuario' && $senha === 'sua_senha') {
+        $_SESSION['logado'] = true;
+        header('Location: menu.php');
+        exit;
+    } else {
+        $mensagemErro = "Credenciais incorretas. Tente novamente.";
+    }
+}
+?>
+
+
+
