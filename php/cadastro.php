09e19a38534a3f554896dedcc0853d84f3e88a51
Juan Pablo <juanpablox2002@gmail.com>
2023-11-09 23:49:08 -0300

    adição de comentários no código

--- php/cadastro.php
+++ php/cadastro.php
@@ -1,7 +1,10 @@
 <?php
-include_once('db_conn.php');
-if(isset($_POST['submit_cadastro'])){
+    include_once('db_conn.php'); // Inclui o arquivo de conexão com o banco de dados
+
+    // Verifica se o formulário foi submetido
+    if(isset($_POST['submit_cadastro'])){
     
+        // Obtém os dados do formulário
         $nome = $_POST['nome'];
         $password = $_POST['password'];
         $email = $_POST['email'];
@@ -9,9 +12,11 @@ if(isset($_POST['submit_cadastro'])){
         $sexo = $_POST['sexo'];
         $data_nasc = $_POST['data_nascimento'];
 
+        // Executa a consulta SQL para inserir os dados na tabela 'usuarios'
         $result = mysqli_query($conexao, "INSERT INTO usuarios (nome, password, email, telefone, sexo, data_nasc) 
-        VALUES('$nome', '$password', '$email', '$telefone', '$sexo', '$data_nasc')");
+            VALUES('$nome', '$password', '$email', '$telefone', '$sexo', '$data_nasc')");
 
+        // Redireciona para a página inicial após o cadastro ser realizado
         header('Location: ../../../project-academic-php/index.html');
         exit;
     }
--- php/db_conn.php
+++ php/db_conn.php
@@ -1,15 +1,17 @@
 <?php
-    $dbHost = "Localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbName = "db_studio";
+    // Configurações do banco de dados
+    $dbHost = "Localhost"; // Host do banco de dados (no caso, local)
+    $dbUsername = "root"; // Nome de usuário do banco de dados
+    $dbPassword = ""; // Senha do banco de dados (neste caso, vazia)
+    $dbName = "db_studio"; // Nome do banco de dados a ser utilizado
 
+    // Cria uma nova conexão com o banco de dados utilizando a classe mysqli
     $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 
+    // Verifica se ocorreu algum erro durante a conexão
     if ($conexao->connect_errno) {
-        echo "Erro";
-    }
-    else{
-        echo "Conexao efetuada com sucesso";
+        echo "Erro ao conectar ao banco de dados"; // Mensagem exibida se houver um erro na conexão
+    } else {
+        echo "Conexão efetuada com sucesso"; // Mensagem exibida se a conexão for estabelecida com sucesso
     }
 ?>
