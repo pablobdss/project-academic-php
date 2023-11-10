a520e19f4234957b68c1e5dc8fe612628d93928d
Juan Pablo <juanpablox2002@gmail.com>
2023-11-09 22:09:33 -0300

    Correções ortográficas

--- php/cadastro.php
+++ php/cadastro.php
@@ -1,14 +1,15 @@
 <?php
 include_once('db_conn.php');
-if(isset($_POST['submit'])){
+if(isset($_POST['submit_cadastro'])){
     
         $nome = $_POST['nome'];
+        $password = $_POST['password'];
         $email = $_POST['email'];
         $telefone = $_POST['telefone'];
         $sexo = $_POST['sexo'];
         $data_nasc = $_POST['data_nascimento'];
 
-        $result = mysqli_query($conexao, "INSERT INTO usuarios (nome, email, telefone, sexo, data_nasc) 
-        VALUES('$nome', '$email', '$telefone', '$sexo', '$data_nasc')");
+        $result = mysqli_query($conexao, "INSERT INTO usuarios (nome, password, email, telefone, sexo, data_nasc) 
+        VALUES('$nome', '$password', '$email', '$telefone', '$sexo', '$data_nasc')");
     }
 ?>
