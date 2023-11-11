14eac3427fe49d00f529064221b980f40e3d8fa5
fabiords07 <142637818+fabiords07@users.noreply.github.com>
2023-11-11 16:43:06 -0300

    parte php do agendamento

--- php/agendamento.php
+++ php/agendamento.php
@@ -1,3 +1,33 @@
 <?php
-echo "vem aí"
+include 'db_conn.php';
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+    $data = $_POST["data"];
+    $hora = $_POST["hora"];
+    $corteCabelo = $_POST["corte_cabelo"];
+    $estiloBarba = $_POST["estilo_barba"];
+
+    //inserir dados na tabela do banco de dados (botei tabela_agendamentos por nao saber o nome dela)
+    $sql = "INSERT INTO tabela_agendamentos (nome, data, hora, corte_cabelo, estilo_barba) VALUES (?, ?, ?, ?, ?)";
+
+    //instrução sql
+    $stmt = $conexao->prepare($sql);
+    if ($stmt) {
+        $stmt->bind_param("sssss", $nome, $data, $hora, $corteCabelo, $estiloBarba);
+        //executar instrução
+        $stmt->execute();
+        if ($stmt->affected_rows > 0) {
+            echo "Agendamento realizado com sucesso!";
+        } else {
+            echo"Erro ao realizar o agendamento";
+        }
+        $stmt->close();
+    } else {
+        echo "Erro na preparação da instrução SQL.";
+    }
+    $conexao->close();
+} else {
+    echo "O formulário não foi submetido corretamente.";
+}
 ?>
