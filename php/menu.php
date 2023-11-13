672f70a23391132bb9405458d56db8bbe4b48b23
Juan Pablo <juanpablox2002@gmail.com>
2023-11-13 02:13:14 -0300

    t

--- php/menu.php
+++ php/menu.php
@@ -1,4 +1,7 @@
 <?php
+    // Inicia ou retoma a sessão
+    session_start();
+
     // Verifica se as variáveis de sessão para e-mail e senha não estão definidas
     if(!isset($_SESSION['email']) || !isset($_SESSION['password'])) {
         // Redireciona para a página de login se o usuário não estiver logado
