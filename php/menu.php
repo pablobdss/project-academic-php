70070f4003b8fb2221a1d581b48aab8bb932a882
Juan Pablo <juanpablox2002@gmail.com>
2023-11-13 01:18:58 -0300

    update

--- php/menu.php
+++ php/menu.php
@@ -5,11 +5,11 @@
     // Verifica se as variáveis de sessão para e-mail e senha não estão definidas
     if(!isset($_SESSION['email']) || !isset($_SESSION['password'])) {
         // Redireciona para a página de login se o usuário não estiver logado
-        header('Location: ../../../project-academic-php/pages/login/login.html');
+        header('Location: ../../../project-academic-php/pages/login/index.html');
         exit; // Garante a interrupção da execução do script após o redirecionamento
     } else {
         // Redireciona para a página do menu se o usuário estiver logado
-        header('Location: ../../../project-academic-php/pages/menu/menu.html');
+        header('Location: ../../../project-academic-php/pages/menu/index.html');
         exit; // Garante a interrupção da execução do script após o redirecionamento
     }
 ?>
