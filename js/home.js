4cdbc743f97c03d8f6f7986a1575a1c905a73d84
fabiords07 <142637818+fabiords07@users.noreply.github.com>
2023-11-13 20:17:58 -0300

    separação ligiosa do código

---
+++ js/home.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var menu = document.getElementById("menu");
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+}
+
+function closeMenu() {
+    var menu = document.getElementById("menu");
+    menu.style.display = "none";
+}
