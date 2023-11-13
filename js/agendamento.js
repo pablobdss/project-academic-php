47e974cfbccecd1803f037ce7748f5b32d119611
Juan Pablo <juanpablox2002@gmail.com>
2023-11-13 20:42:57 -0300

    integração ao BD e alteração total do agendamento

--- js/agendamento.js
+++ js/agendamento.js
@@ -1,70 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('form');
-    form.addEventListener('submit', function (event) {
-        const corteCabelo = document.getElementById('corte-cabelo').value;
-        const estiloBarba = document.getElementById('barba').value;
-
-        if (corteCabelo === 'nenhum' && estiloBarba === 'nenhum') {
-            alert('Escolha pelo menos um corte de cabelo ou um estilo de barba.');
-            event.preventDefault();
-        }
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Obtendo referência aos elementos do formulário
-    const horaInput = document.getElementById('hora');
-    const selectMinutos = document.getElementById('minutos');
-
-    // Definindo os horários disponíveis
-    const horarios = [
-        { inicio: 8, fim: 12 },  // Horários da manhã
-        { inicio: 14, fim: 19 }, // Horários da tarde
-    ];
-
-    // Incremento em minutos para as opções do select
-    const minutoIncremento = 10;
-
-    // Iterando sobre os horários
-    for (const horario of horarios) {
-        for (let hora = horario.inicio; hora <= horario.fim; hora++) {
-            // Definindo o limite máximo de minutos para a última hora
-            const maxMinuto = (hora === horario.fim) ? 60 : 60;
-
-            // Iterando sobre os minutos com o incremento especificado
-            for (let minuto = 0; minuto < maxMinuto; minuto += minutoIncremento) {
-                // Formatando a hora e o minuto com zero à esquerda, se necessário
-                const horaFormatada = hora.toString().padStart(2, '0');
-                const minutoFormatado = minuto.toString().padStart(2, '0');
-
-                // Criando uma opção para o elemento select
-                const opcao = document.createElement('option');
-                opcao.value = `${horaFormatada}:${minutoFormatado}`;
-                opcao.textContent = `${horaFormatada}:${minutoFormatado}`;
-
-                // Adicionando a opção ao menu suspenso de minutos
-                selectMinutos.appendChild(opcao);
-            }
-        }
-    }
-
-    // Definindo um valor padrão para os minutos
-    selectMinutos.value = '08:00';
-
-    // Adicionando um ouvinte de evento para ajustar a hora ao alterar os minutos
-    selectMinutos.addEventListener('change', function () {
-        horaInput.value = selectMinutos.value;
-    });
-
-    // Adicionando um ouvinte de evento ao alterar a hora diretamente
-    horaInput.addEventListener('input', function () {
-        selectMinutos.value = horaInput.value;
-    });
-});
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    const dataInput = document.getElementById('data');
+    const dataInput = document.getElementById('data_agendamento');
 
     // Obter a data de hoje
     const hoje = new Date();
@@ -79,18 +14,31 @@ document.addEventListener('DOMContentLoaded', function () {
     const dataFormatada = `${ano}-${mes}-${dia}`;
 
     // Configurar a data mínima
-    dataInput.min = dataFormatada;
+    if (dataInput) {
+        dataInput.min = dataFormatada;
 
-    // Adicionar um ouvinte de evento ao selecionar a data
-    dataInput.addEventListener('input', function () {
-        const dataSelecionada = new Date(dataInput.value);
-        const diaSemana = dataSelecionada.getDay();
+        // Adicionar um ouvinte de evento ao selecionar a data
+        dataInput.addEventListener('input', function () {
+            const dataSelecionada = new Date(dataInput.value);
+            const diaSemana = dataSelecionada.getDay();
 
-        // Dia da semana 6 é sábado e o dia da semana 0 é domingo
-        if (diaSemana === 5 || diaSemana === 6) {
-            alert('Escolha uma data que não seja sábado ou domingo.');
-            dataInput.value = ''; // Limpar o valor do input
-        }
-    });
+            // Dia da semana 6 é sábado e o dia da semana 0 é domingo
+            if (diaSemana === 5 || diaSemana === 6) {
+                alert('Escolha uma data que não seja sábado ou domingo.');
+                dataInput.value = ''; // Limpar o valor do input
+            }
+        });
+    }
 });
 
+document.getElementById('valida_form').addEventListener('submit', function (event) {
+    console.log('Script executado');
+    var corteCabelo = document.getElementById('tipos_cortes').value;
+    var estiloBarba = document.getElementById('tipos_barbas').value;
+
+    if (corteCabelo === 'nenhum' && estiloBarba === 'nenhum') {
+        alert('Por favor, escolha pelo menos um corte de cabelo ou um estilo de barba.');
+        event.preventDefault(); // Impede o envio do formulário
+    }
+    alert("Agendamento realizado com Sucesso!")
+});
--- php/agendamento.php
+++ php/agendamento.php
@@ -1,33 +1,60 @@
 <?php
-include 'db_conn.php';
+// Inclua o arquivo de conexão
+include_once('db_conn.php');
+include_once('verifica_login.php');
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST["nome"];
-    $data = $_POST["data"];
-    $hora = $_POST["hora"];
-    $corteCabelo = $_POST["corte_cabelo"];
-    $estiloBarba = $_POST["estilo_barba"];
+// Inicializa a resposta como um array
+$response = array();
 
-    //inserir dados na tabela do banco de dados (botei tabela_agendamentos por nao saber o nome dela)
-    $sql = "INSERT INTO tabela_agendamentos (nome, data, hora, corte_cabelo, estilo_barba) VALUES (?, ?, ?, ?, ?)";
+// Verifica se o formulário foi submetido
+if(isset($_POST['submit_agendamento'])){
+    // Obtenha os dados do formulário
+    $data_agendada = mysqli_real_escape_string($conexao, $_POST['data_agendamento']);
+    $hora_agendada = mysqli_real_escape_string($conexao, $_POST['horarios']);
+    $corte_esc = mysqli_real_escape_string($conexao, $_POST['tipos_cortes']);
+    $barba_esc = mysqli_real_escape_string($conexao, $_POST['tipos_barbas']);
 
-    //instrução sql
+    // Prepare a instrução SQL para a inserção
+    $sql = "INSERT INTO agendamentos (data_agendada, hora_agendada, corte_esc, barba_esc) VALUES (?, ?, ?, ?)";
+
+    // Prepare a declaração
     $stmt = $conexao->prepare($sql);
+
+    // Verifica se a preparação da declaração foi bem-sucedida
     if ($stmt) {
-        $stmt->bind_param("sssss", $nome, $data, $hora, $corteCabelo, $estiloBarba);
-        //executar instrução
+        // Vincule os parâmetros e execute a declaração
+        $stmt->bind_param('ssss', $data_agendada, $hora_agendada, $corte_esc, $barba_esc);
         $stmt->execute();
+
+        // Verifica se a execução foi bem-sucedida
         if ($stmt->affected_rows > 0) {
-            echo "Agendamento realizado com sucesso!";
+            // Redireciona para o menu
+            header('Location: ../menu/menu.html');
+            exit;
         } else {
-            echo"Erro ao realizar o agendamento";
+            $response['success'] = false;
+            $response['error'] = 'Erro ao inserir no banco de dados. Detalhes: ' . $stmt->error;
         }
+
+        // Feche a declaração
         $stmt->close();
     } else {
-        echo "Erro na preparação da instrução SQL.";
+        $response['success'] = false;
+        $response['error'] = 'Erro na preparação da declaração. Detalhes: ' . $conexao->error;
     }
+
+    // Feche a conexão
     $conexao->close();
 } else {
-    echo "O formulário não foi submetido corretamente.";
+    $response['success'] = false;
+    $response['error'] = 'Formulario nao submetido corretamente.';
 }
+
+// Remova qualquer saída anterior
+ob_clean();
+
+// Envia a resposta como JSON
+header('Content-Type: application/json');
+echo json_encode($response);
+exit;
 ?>
