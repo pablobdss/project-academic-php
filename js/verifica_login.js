4858c6918ae62a6ff1838119b5c0f014c26e3310
Juan Pablo <juanpablox2002@gmail.com>
2023-11-15 22:56:19 -0300

    update

--- js/verifica_login.js
+++ js/verifica_login.js
@@ -1,41 +1,24 @@
 // Esconde o conteúdo protegido inicialmente
 $('#conteudoProtegido').hide();
 
-// Função para verificar a autenticação
-function verificarAutenticacao() {
+$(function () {
     fetch('../../php/verifica_autenticacao.php')
         .then(response => response.json())
         .then(data => {
-            console.log("olaa")
             if (!data.isAuthenticated) {
                 window.location.href = '../../pages/login/login.html';
             } else {
-                mostrarConteudoAutenticado(); // Mostra o conteúdo autenticado
-                atualizarMenu(true); // Atualiza o menu com autenticação verdadeira
+                $('#conteudoProtegido').show();
+                $('#cadastroLink').hide();
+                $('#loginLink').hide();
+                $('#menuLink').show()
             }
         })
         .catch(error => {
             console.error('Erro na verificação de autenticação:', error);
         });
-}
-
-function mostrarConteudoAutenticado() {
-    $('#conteudoProtegido').show(); // Mostra o conteúdo autenticado
-}
-
-function atualizarMenu(isAuthenticated) {
-    if (isAuthenticated) {
-        $('#cadastroLink, #loginLink').hide();
-        $('#menuLink').show();
-    } else {
-        $('#cadastroLink, #loginLink').show();
-        $('#menuLink').hide();
-    }
-}
-
-$(function () {
-    verificarAutenticacao();
 });
+
 
 // Função para fazer logout
 function logout() {
