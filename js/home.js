a29989975238d03a17396f9729297436c591680f
Juan Pablo <juanpablox2002@gmail.com>
2023-11-15 22:22:03 -0300

    home.js adicionado verificador de autentic

--- js/home.js
+++ js/home.js
@@ -7,3 +7,19 @@ function closeMenu() {
     var menu = document.getElementById("menu");
     menu.style.display = "none";
 }
+
+function verificarAutenticacao() {
+    fetch('../../php/verifica_autenticacao.php')
+        .then(response => response.json())
+        .then(data => {
+            // Se o usuário não estiver autenticado, redirecione para a página de login
+            if (!data.isAuthenticated) {
+                $('#cadastroLink, #loginLink').show(); // Mostra os links de Cadastro e Login
+                $('#menuLink').hide();
+            } else {
+                // Mostra o conteúdo protegido
+                $('#cadastroLink, #loginLink').hide(); // Esconde os links de Cadastro e Login
+                $('#menuLink').show()
+            }
+        })
+}
