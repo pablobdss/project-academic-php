c34e52c4c3cba92e7b19f045ebafa0da9fe9d724
Juan Pablo <juanpablox2002@gmail.com>
2023-11-15 23:47:02 -0300

    nova logica com ajax pro home.js

--- js/home.js
+++ js/home.js
@@ -8,18 +8,27 @@ function closeMenu() {
     menu.style.display = "none";
 }
 
-function verificarAutenticacao() {
-    fetch('../../php/verifica_autenticacao.php')
-        .then(response => response.json())
-        .then(data => {
-            // Se o usuário não estiver autenticado, redirecione para a página de login
-            if (!data.isAuthenticated) {
-                $('#cadastroLink, #loginLink').show(); // Mostra os links de Cadastro e Login
-                $('#menuLink').hide();
+$(function() {
+    // Faz a requisição AJAX para verificar a autenticação
+    $.ajax({
+        url: '../../php/verifica_autenticacao.php',
+        type: 'GET',
+        dataType: 'json',
+        success: function(response) {
+            if (response.isAuthenticated) {
+                // Usuário autenticado, oculta o link de cadastro e login, exibe o link do menu
+                $('#cadastroLink').hide();
+                $('#loginLink').hide();
+                $('#menuLink').show();
             } else {
-                // Mostra o conteúdo protegido
-                $('#cadastroLink, #loginLink').hide(); // Esconde os links de Cadastro e Login
-                $('#menuLink').show()
+                // Usuário não autenticado, mantém os links de cadastro e login visíveis, esconde o link do menu
+                $('#cadastroLink').show();
+                $('#loginLink').show();
+                $('#menuLink').hide();
             }
-        })
-}
+        },
+        error: function(xhr, status, error) {
+            console.error('Erro ao verificar autenticação:', error);
+        }
+    });
+});
