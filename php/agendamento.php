4d97737b8627ae48e18a6e4d22d7f0b7decaeb2f
Juan Pablo <juanpablox2002@gmail.com>
2023-11-15 22:20:42 -0300

    Sistema de agendamento consertado

--- php/agendamento.php
+++ php/agendamento.php
@@ -1,59 +1,53 @@
 <?php
-// Inclua o arquivo de conexão
+// Inclui o arquivo de conexão
 include_once('verifica_login.php');
 include_once('db_conn.php');
 
-// Inicializa a resposta como um array
 $response = array();
 
-// Verifica se o formulário foi submetido
-if(isset($_POST['submit_agendamento'])){
-    // Obtenha os dados do formulário
+if (isset($_POST['submit_agendamento'])) {
     $data_agendada = mysqli_real_escape_string($conexao, $_POST['data_agendamento']);
     $hora_agendada = mysqli_real_escape_string($conexao, $_POST['horarios']);
     $corte_esc = mysqli_real_escape_string($conexao, $_POST['tipos_cortes']);
     $barba_esc = mysqli_real_escape_string($conexao, $_POST['tipos_barbas']);
 
+    session_start(); // Inicia a sessão se ainda não foi iniciada
+    $user_id = $_SESSION['id']; // Obtém o user_id da sessão
+
     // Prepare a instrução SQL para a inserção
-    $sql = "INSERT INTO agendamentos (data_agendada, hora_agendada, corte_esc, barba_esc) VALUES (?, ?, ?, ?)";
+    $sql = "INSERT INTO agendamentos (data_agendada, hora_agendada, corte_esc, barba_esc, user_id) VALUES (?, ?, ?, ?, ?)";
 
     // Prepare a declaração
     $stmt = $conexao->prepare($sql);
 
-    // Verifica se a preparação da declaração foi bem-sucedida
     if ($stmt) {
-        // Vincule os parâmetros e execute a declaração
-        $stmt->bind_param('ssss', $data_agendada, $hora_agendada, $corte_esc, $barba_esc);
+        $stmt->bind_param('ssssi', $data_agendada, $hora_agendada, $corte_esc, $barba_esc, $user_id);
         $stmt->execute();
 
-        // Verifica se a execução foi bem-sucedida
         if ($stmt->affected_rows > 0) {
-            // Redireciona para o menu
-            header('Location: ../menu/menu.html');
+            $response['success'] = true;
+            $response['message'] = 'Agendamento realizado com sucesso!';
+            header('Location: ../../../project-academic-php/pages/menu/menu.html');
+            include('atualiza_agendamentos.php');
             exit;
         } else {
             $response['success'] = false;
             $response['error'] = 'Erro ao inserir no banco de dados. Detalhes: ' . $stmt->error;
         }
 
-        // Feche a declaração
         $stmt->close();
     } else {
         $response['success'] = false;
         $response['error'] = 'Erro na preparação da declaração. Detalhes: ' . $conexao->error;
     }
 
-    // Feche a conexão
     $conexao->close();
 } else {
     $response['success'] = false;
-    $response['error'] = 'Formulario nao submetido corretamente.';
+    $response['error'] = 'Formulário não submetido corretamente.';
 }
 
-// Remova qualquer saída anterior
 ob_clean();
-
-// Envia a resposta como JSON
 header('Content-Type: application/json');
 echo json_encode($response);
 exit;
