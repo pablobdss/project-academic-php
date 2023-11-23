052d276f5e9d6765a8a95f2a9677226267a2e377
Juan Pablo <juanpablox2002@gmail.com>
2023-11-23 06:34:14 -0300

    SQL Update

--- SQL/db_studio.sql
+++ SQL/db_studio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 16/11/2023 às 05:42
+-- Tempo de geração: 23/11/2023 às 10:33
 -- Versão do servidor: 10.4.28-MariaDB
 -- Versão do PHP: 8.2.4
 
@@ -36,13 +36,17 @@ CREATE TABLE `agendamentos` (
   `barba_esc` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Despejando dados para a tabela `agendamentos`
+-- Estrutura para tabela `opcoes_agendamento`
 --
 
-INSERT INTO `agendamentos` (`agendamento_id`, `user_id`, `data_agendada`, `hora_agendada`, `corte_esc`, `barba_esc`) VALUES
-(48, 1, '2023-11-29', '09:10', 'quiff', 'curta'),
-(49, 1, '2023-11-29', '08:00', 'pompadour', 'nenhum');
+CREATE TABLE `opcoes_agendamento` (
+  `id` int(5) NOT NULL,
+  `tipo` varchar(50) NOT NULL,
+  `nome` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -61,16 +65,6 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Despejando dados para a tabela `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `nome`, `password`, `email`, `telefone`, `sexo`, `data_nasc`) VALUES
-(1, 'Pedro', '1234', 'pedrinhosousa077@gmail.com', '83993451591', 'masculino', '2023-11-14'),
-(2, 'teste', '1234', 'teste@gmail.com', '2131421412412', 'masculino', '2002-05-16'),
-(3, 'sdada', 'abc', 'testee@gmail.com', '+55 (83) 9 321-31231', 'masculino', '2002-06-16'),
-(4, 'dasdas', 'dasdsadas', 'pedrinhosousa07743@gmail.com', '231414124214124', 'masculino', '2023-11-16');
-
---
 -- Índices para tabelas despejadas
 --
 
@@ -82,10 +76,19 @@ ALTER TABLE `agendamentos`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Índices de tabela `opcoes_agendamento`
+--
+ALTER TABLE `opcoes_agendamento`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_horarios` (`tipo`,`nome`);
+
+--
 -- Índices de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_email` (`email`),
+  ADD UNIQUE KEY `unique_telefone` (`telefone`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -95,13 +98,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `agendamentos`
 --
 ALTER TABLE `agendamentos`
-  MODIFY `agendamento_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `agendamento_id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `opcoes_agendamento`
+--
+ALTER TABLE `opcoes_agendamento`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restrições para tabelas despejadas
