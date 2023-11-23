61b3753fb379ac9f8e23d1706065f8a7cad5ad40
fabiords07 <142637818+fabiords07@users.noreply.github.com>
2023-11-23 07:48:26 -0300

    atualização home.js

--- js/home.js
+++ js/home.js
@@ -1,24 +1,43 @@
 $(function() {
     // Faz a requisição AJAX para verificar a autenticação
     $.ajax({
-        url: '../../php/verifica_autenticacao.php',
+        url: '../project-academic-php/php/verifica_autenticacao.php',
         type: 'GET',
         dataType: 'json',
         success: function(response) {
+            // Limpa o conteúdo atual do menu
+            $('#menuList').empty();
+
             if (response.isAuthenticated) {
-                // Usuário autenticado, oculta o link de cadastro e login, exibe o link do menu
-                $('#cadastroLink').hide();
-                $('#loginLink').hide();
-                $('#menuLink').show();
+                // Usuário autenticado, adiciona links específicos para usuário logado
+                addMenuItem('HOME', 'index.html', 'paginaInicial');
+                addMenuItem('MENU', 'pages/menu/menu.html', 'menuLink');
             } else {
-                // Usuário não autenticado, mantém os links de cadastro e login visíveis, esconde o link do menu
-                $('#cadastroLink').show();
-                $('#loginLink').show();
-                $('#menuLink').hide();
+                // Usuário não autenticado, adiciona links específicos para usuário não logado
+                addMenuItem('HOME', 'index.html', 'paginaInicial');
+                addMenuItem('CADASTRO', 'pages/cadastro/cadastro.html', 'cadastroLink');
+                addMenuItem('LOGIN', 'pages/login/login.html', 'loginLink');
             }
         },
         error: function(xhr, status, error) {
             console.error('Erro ao verificar autenticação:', error);
         }
     });
+
+    // Função para adicionar itens ao menu dinamicamente
+    function addMenuItem(text, href, id) {
+        // Cria um novo elemento <li> e <a>
+        var newItem = $('<li><a></a></li>');
+
+        // Define os atributos do link (<a>)
+        newItem.find('a')
+            .attr('href', href)
+            .attr('id', id)
+            .addClass('menu-link')
+            .text(text);
+
+        // Adiciona o novo item ao menu
+        $('#menuList').append(newItem);
+    }
 });
+
